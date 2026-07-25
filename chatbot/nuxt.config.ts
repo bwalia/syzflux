3b522c02b726adeacd91167ce22462fa7e29294f
GitLab Automation <automation@gitlab.local>
2026-07-25 12:04:28 +0100

    Old code deleted

--- chatbot/nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from '@nuxt/bridge';
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['nuxt-icon']
-})
